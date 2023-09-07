1b0745e90f325327df03a7fe5932d37159f4f7c8
MGutix <140160200+MGutix@users.noreply.github.com>
2023-09-07 20:12:28 +0900

    Some bux fixes + Better Var Names

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
+function add(num1, num2) {return num1 + num2 };
+function substract(num1, num2) {return num1 - num2 };
+function multiply(num1, num2) {return num1 * num2 };
+function divide(num1, num2) {return num1 / num2 };
+
 function operate(x, op, y){
     x = +x
     y = +y
     if (op === '+') {
-        return x + y;
+        return add(x, y);
     } else if (op === '-') {
-        return x - y;
+        return substract(x, y);
     } else if (op === '*') {
-        return x * y;
+        return multiply(x, y);
     } else if (op === '/') {
-        return x / y;
+        return divide(x, y);
     }
 }
 
+function clearMainDisplay(){
+    display.textContent = ''
+}
+
+function clearAll(){
+    display.textContent = ''
+    secondaryDisplay.textContent = ''
+    num1 = undefined
+    num2 = undefined
+    op = undefined
+}
+
+const clearButton = document.getElementById('clear')
+clearButton.addEventListener("mouseup", clearAll);
 const keylog = document.querySelector("body");
 const display = document.getElementById('display')
+const secondaryDisplay = document.getElementById('secondaryDisplay')
 keylog.addEventListener("keypress", logKey);
 
-let num1;
-let num2;
-let op;
-let result;
+
+let num1 = undefined;
+let num2 = undefined;
+let op = undefined;
+let result = undefined;
 
 function logKey(e) {
     const regEx = /[0-9]/g;
@@ -27,49 +48,46 @@ function logKey(e) {
     if (regEx.test(e.key) == true){
         display.textContent += `${e.key}`;
 
+        
+    } else if (['+', '-', '*', '/', 'Enter'].includes(e.key)) {
+
         if (num1 === undefined) {
             num1 = display.innerHTML;
         } else if (num1 != undefined){
             num2 = display.innerHTML;
-        }
-
-    } else if (['+', '-', '*', '/', 'Enter'].includes(e.key)) {
+        }        
         
-        
-        if (e.key == 'Enter') {
-            clear();
+        if (op === undefined){
+            op = e.key;
+            clearMainDisplay();
+            secondaryDisplay.textContent = `${num1} ${op}`
+        } else if (num1 != undefined && num2 != undefined) {
+            clearMainDisplay();
             result = operate(num1, op, num2);
             display.textContent = result
-            num1 = result
-            num2 = undefined
-        } else{
-            if (num1 != undefined && num2 != undefined) {
-                clear();
-                result = operate(num1, op, num2);
-                display.textContent = result
-                num1 = result
+
+            if(e.key == 'Enter'){
+                secondaryDisplay.textContent = `${num1} ${op} ${num2} =`
+                op  = undefined
+                num1 = undefined
                 num2 = undefined
+            } else {
+                num1 = result
+                secondaryDisplay.textContent = `${num1} ${op}`
+                clearMainDisplay();
+                op  = undefined
             }
-            op = e.key;
-            clear();
-        }
+            num2 = undefined
+        }   
         
-    }
+        
+        
+     }
     
 };
 
-
-const clearButton = document.getElementById('clear')
-clearButton.addEventListener("mouseup", clearAll);
-
-function clear(){
-    display.textContent = ''
-}
-
-function clearAll(){
-    display.textContent = ''
-    num1 = undefined
-    num2 = undefined
-}
-
-
+// User types again (if user types other op before number change op to new one) save Num2
+// When op appears:
+    //OPERATE!
+    // if op is = show in secondary display '45 + 5 =' and in main result
+    // if op is NOT = show in secondary display 'result +' and in main result
